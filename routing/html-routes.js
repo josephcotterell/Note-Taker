5462a681a299dfa19862d4539704d554896b88f3
joseph <josephcotterell@hotmail.co.uk>
2023-01-31 17:20:20 +0000

    fixed some api bugs

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,11 +1,20 @@
 const path = require("path");
+const router = require("express").Router();
 
+/*
 module.exports = function (app) {
   app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname + "/../public/notes.html"));
   });
 };
+*/
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/../public/index.html"));
+router.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname + "/../Develop/public/notes.html"));
 });
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/..Develop/public/index.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 //Dependencies
 const express = require("express");
 const path = require("path");
+const htmlRoutes = require("./routing/html-routes");
+const apiRoutes = require("./routing/api-routes");
 
 //sets up Express
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // sets up the Express app to handle data parsing
-app.use(express.urlencoded({extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 //parse app
-app.use(express.json {});
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname + '/app/pubic')))
+app.use(express.static(path.join(__dirname + "/Develop/public")));
+//app.use(express.static("public"));
 
-require("./app/routing/api-routes.js")(app);
+/*
+require("./routing/api-routes.js")(app);
 
-require("./app/routing/html-routes.js")(app);
+require("./routing/html-routes.js")(app);
+*/
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.listen(PORT, function(){
-    console.log('App listening on PORT: ' + PORT)
-})
-
-
-
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
